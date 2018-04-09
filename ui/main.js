2c82c0ca246d253569d1b6c972a21eb4f14a3126
Kritika Suman <32403544+Kritika-Suman@users.noreply.github.com>
2018-04-09 19:29:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,23 @@ button.onclick = function(){
     
 };
 
-
+//Submit name
+var nameInput = document.getElementById('name');
+var name =  nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //Make arequest to the server and send the name
+    
+    //Compare a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list ='';
+    for(var i=0, i<names.length; i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    vr ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+    
+};    
 
 
 
