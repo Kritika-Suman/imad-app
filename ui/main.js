29de11ba5ed58876b0885a2b8eeac29674be4c6f
Kritika Suman <32403544+Kritika-Suman@users.noreply.github.com>
2018-04-09 20:32:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,9 +35,12 @@ button.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                //var counter = request.responseText;
+                //var span = document.getElementById('count');
+                //span.innerHTML = counter.toString();
+                
+                
+                
             }
         }
         //Not done yet
@@ -57,22 +60,53 @@ button.onclick = function(){
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name =  nameInput.value;
+//var nameInput = document.getElementById('name');
+//var name =  nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make arequest to the server and send the name
     
-    //Compare a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list ='';
-    for(var i=0; i<names.length; i++){
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
     
-};    
+    //Compare a list of names and render it as a list
+    //var names = ['name1','name2','name3','name4'];
+    //var list ='';
+    //for(var i=0; i<names.length; i++){
+        //list += '<li>'+names[i]+'</li>';
+    //}
+    //var ul = document.getElementById('nameList');
+    //ul.innerHTML = list;
+    
+    
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+         
+           //Compare a list of names and render it as a list
+          var names =request.responseText;
+          names = JSON.parse(names);
+          var list ='';
+          for(var i=0; i<names.length; i++){
+          list += '<li>'+names[i]+'</li>';
+          }
+          var ul = document.getElementById('nameList');
+          ul.innerHTML = list;
+    
+          
+        }
+        //Not done yet
+};  
+
+
+   //Make the request
+   var nameInput = document.getElementByID('name');
+   var name = nameInput.value;
+    request.open('GET','http://kritikasuman2016.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+    
 
 
 
