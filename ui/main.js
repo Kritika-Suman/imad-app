b07953d1168fe46e202448fb2bb8a47b9419ac2d
Kritika Suman <32403544+Kritika-Suman@users.noreply.github.com>
2018-04-09 19:30:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ submit.onclick = function(){
     for(var i=0, i<names.length; i++){
         list += '<li>'+names[i]+'</li>';
     }
-    vr ul = document.getElementById('nameList');
+    var ul = document.getElementById('nameList');
     ul.innerHTML = list;
     
 };    
